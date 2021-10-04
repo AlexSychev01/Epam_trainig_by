04f9d4ed7e7364865566f67eab06b671e735df73
Aleksandr Sychev <alex39ksg@mail.ru>
2021-10-04 22:59:25 +0200

    first commit

--- src/main/java/Programm.java
+++ src/main/java/Programm.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 public class Programm {
     public static void main(String[] args) {
         Numbers number = new Numbers();
-        Scanner scanner = new Scanner(System.in);
-        int minNum = number.min(2,5,6);
-        int maxNum = number.max(2,5,6);
+
+        int minNum = number.min(2, 5, 6);
+        int maxNum = number.max(2, 5, 6);
         System.out.println("Сумма наибольшего и наименьшего значений равна: " + minNum + maxNum);
 
     }
